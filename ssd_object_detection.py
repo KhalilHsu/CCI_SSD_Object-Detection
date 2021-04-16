b8eeb8845378b80d3c89e4a435250c9acbb4fa99
Khalil <65265303+KhalilHsu@users.noreply.github.com>
2021-04-16 02:18:10 +0100

    Add files via upload

---
+++ ssd_object_detection.py
@@ -0,0 +1,68 @@
+# python ssd_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+import numpy as np
+import cv2
+import argparse
+import imutils
+from imutils.video import VideoStream
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+print("starting video stream...")
+vs = VideoStream(src=0).start()
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it to have a maximum width of 800 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=800)
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+	net.setInput(blob)
+	detections = net.forward()
+
+	# object detection
+	for i in np.arange(0, detections.shape[2]):
+		confidence = detections[0, 0, i, 2]
+		if confidence > args["confidence"]:
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# draw the rectangle on the frame
+			label = "{}: {:.2f}%".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord("q"):
+		break
+
+cv2.destroyAllWindows()
+vs.stop()
+
